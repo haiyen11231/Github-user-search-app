6e801d9ad56d9eb74113a0d169feefc1e453dae6
haiyen11231 <haiyen11231@gmail.com>
2024-02-18 12:24:52 +0800

    feat: ui navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <main>
       <div className="home__container">
+        <section className="navbar">
+          <h1 className="navbar__title">devfinder</h1>
+          <button className="navbar__btn">LIGHT</button>
+        </section>
         <SearchSection></SearchSection>
       </div>
     </main>
