1081775630662337bf74280081c39885d046eba7
haiyen11231 <haiyen11231@gmail.com>
2024-03-08 00:43:58 +0800

    fix: joining date & media sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,26 @@ import { useState } from "react";
 export default function Home() {
   const [username, setUsername] = useState<string>("");
   const [result, setResult] = useState({});
+  const [displayInfoCard, setDisplayInfoCard] = useState<boolean>(false);
   const handleSearch = async () => {
     try {
       const res = await fetch(`https://api.github.com/users/${username}`);
       // console.log(res);
-      const data = await res.json();
-      console.log(data);
-      setResult(data);
-    } catch (error) {}
+      if (res.status === 200) {
+        const data = await res.json();
+        console.log(data);
+        setResult(data);
+        setDisplayInfoCard(true);
+      } else {
+        setDisplayInfoCard(false);
+        setResult({});
+      }
+
+      // setDisplayInfoCard(true);
+    } catch (error) {
+      setDisplayInfoCard(false);
+      console.log("displayInfoCard", displayInfoCard);
+    }
   };
   return (
     <main>
@@ -25,21 +37,26 @@ export default function Home() {
           onChange={(e) => setUsername(e.target.value)}
           onClick={handleSearch}
         />
-        <InfoCard
-          src={result?.avatar_url}
-          name={result?.name}
-          href={result?.html_url}
-          date={result?.updated_at}
-          bio={result?.bio}
-          repo={result?.public_repos}
-          followers={result?.followers}
-          following={result?.following}
-          location={result?.location}
-          blog={result?.blog}
-          twitter={result?.twitter_username}
-          company={result?.company}
-          userName={result?.login}
-        />
+        {displayInfoCard && (
+          <InfoCard
+            src={result?.avatar_url}
+            name={result?.name}
+            href={result?.html_url}
+            date={result?.updated_at}
+            bio={result?.bio}
+            repo={result?.public_repos}
+            followers={result?.followers}
+            following={result?.following}
+            location={result?.location}
+            blog={result?.blog}
+            twitter={result?.twitter_username}
+            company={result?.company}
+            userName={result?.login}
+          />
+        )}
+        {username && !displayInfoCard && (
+          <div className="error">User not found</div>
+        )}
       </div>
     </main>
   );
